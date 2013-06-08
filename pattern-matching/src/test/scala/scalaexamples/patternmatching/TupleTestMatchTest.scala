84aab50f0eedbc8ffa3861f4c8744e40bbaea45d
emoen <endremoen@hotmail.com>
2013-06-08 11:26:02 +0200

    completed tuples

--- pattern-matching/src/test/scala/scalaexamples/patternmatching/TupleTestMatchTest.scala
+++ pattern-matching/src/test/scala/scalaexamples/patternmatching/TupleTestMatchTest.scala
@@ -5,39 +5,42 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 import scalaexamples.EmptyTest
+import java.lang.Boolean
 
 @RunWith(classOf[JUnit4])
 class TupleTestMatchTest extends EmptyTest {
   
-  // @Test
+  @Test
   def matchOnTupes {
     val simpleTuple: Any = (1, "string")
 
     simpleTuple match {
       // Match on the tuple, and assert that its the same => assertEquals(simpleTuple, (one, two))
+      case (one, two) => assertEquals(simpleTuple, (one,two))
       case _ => sys.error("Couldnt match it")
     }
   }
   
-  // @Test
+  @Test
   def matchOnTupesWithGuard {
     val simpleTuple: Any = (1, "string")
 
     simpleTuple match {
-      case (one, _) => assertEquals(2, one)
+      case (one, _) if ( one == 2) => assertEquals(2, one)
       // Comment in this line and make this run, (hint create guard on the previous match, so that it won't hit)
-      // case (one, _) => assertEquals(1, one)
+      case (one, _) => assertEquals(1, one)
       case _ => sys.error("Couldnt match it")
     }
   }
 
-  // @Test
+  @Test
   def matchOnLargerTuple{
     val simpleTuple: Any = (1, "string", false)
     
     simpleTuple match {
       // Match on the tuple, and assert with assertFalse(three)
       case (one, two, three: String) => sys.error("this is not a Tuple3 with last element Boolean")
+      case (one: Int, two: String, three: Boolean) => assertFalse(three)
       case _ => sys.error("Couldnt match it")
     }
   }
